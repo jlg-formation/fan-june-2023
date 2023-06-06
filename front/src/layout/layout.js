281a87ce53f223d48c411d706c5dc4977362a9c7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-06 11:23:43 +0200

    dev: added webpack html plugin

--- front/src/layout/layout.js
+++ front/src/layout/layout.js
@@ -1,25 +1,21 @@
-(function (window) {
-  "use strict";
+const app = angular.module("layout", []);
 
-  const app = angular.module("layout", []);
+app.config(function ($rootScopeProvider) {
+  console.log("config layout");
+});
+app.run(function ($rootScope) {
+  console.log("$rootScope: ", $rootScope);
+  console.log("run layout");
+});
 
-  app.config(function ($rootScopeProvider) {
-    console.log("config layout");
-  });
-  app.run(function ($rootScope) {
-    console.log("$rootScope: ", $rootScope);
-    console.log("run layout");
-  });
+app.component("layoutHeader", {
+  templateUrl: "./layout/layout-header/layout-header.component.html",
+});
 
-  app.component("layoutHeader", {
-    templateUrl: "./layout/layout-header/layout-header.component.html",
-  });
+app.component("layoutBody", {
+  template: "<p>Body works!</p>",
+});
 
-  app.component("layoutBody", {
-    template: "<p>Body works!</p>",
-  });
-
-  app.component("layoutFooter", {
-    template: "<p>Footer works!</p>",
-  });
-})(window);
+app.component("layoutFooter", {
+  template: "<p>Footer works!</p>",
+});
--- front/src/main.js
+++ front/src/main.js
@@ -1,17 +1,15 @@
-(function (window) {
-  "use strict";
+import "./layout/layout";
 
-  var app = angular.module("myApp", ["layout"]);
-  app.config(function () {
-    console.log("config myApp");
-  });
-  app.run(function () {
-    console.log("run myApp");
-  });
-  app.component("hello", {
-    template: "<h1>Hello {{ name }}</h1>",
-    controller: function ($scope) {
-      $scope.name = "JLG";
-    },
-  });
-})(window);
+var app = angular.module("myApp", ["layout"]);
+app.config(function () {
+  console.log("config myApp");
+});
+app.run(function () {
+  console.log("run myApp");
+});
+app.component("hello", {
+  template: "<h1>Hello {{ name }}</h1>",
+  controller: function ($scope) {
+    $scope.name = "JLG";
+  },
+});
