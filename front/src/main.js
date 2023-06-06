0a8cf94ce355bae8dc99dc3c4a140e1e762db0ff
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-06 14:47:49 +0200

    dev: added app component

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,7 @@
 import "angular";
 
+import appHtml from "./app/app.component.html";
+
 import "./layout/layout";
 
 import "./style.scss";
@@ -11,9 +13,6 @@ app.config(function () {
 app.run(function () {
   console.log("run myApp");
 });
-app.component("hello", {
-  template: "<h1>Hello {{ name }}</h1>",
-  controller: function ($scope) {
-    $scope.name = "JLG";
-  },
+app.component("app", {
+  template: appHtml,
 });
