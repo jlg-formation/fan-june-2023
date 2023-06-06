56ecbe0b2efca9da601e953d2a8d168b0f3dfa40
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-06 11:57:36 +0200

    dev: added mode

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/main.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+module.exports = function (_, args) {
+  const mode = args.mode || "production";
+  return {
+    mode: mode,
+    entry: "./src/main.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devtool: mode === "development" ? "source-map" : false,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+  };
 };
