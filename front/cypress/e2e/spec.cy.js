d11e0d1a6c65f580d1cc2301ce37d6e5a219d0b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-07 15:32:07 +0200

    dev: added test 2e2

--- front/cypress/e2e/spec.cy.js
+++ front/cypress/e2e/spec.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:9000");
+
+    cy.contains("Gestion Stock");
+    cy.contains("Mentions Légales");
+    cy.contains("a", "Voir le stock").click();
+
+    cy.get("a[title='Ajouter']").click();
+
+    const uuid = () => Cypress._.random(0, 1e6);
+    const id = uuid();
+    const name = `outil-${id}`;
+
+    cy.get("input").eq(0).clear().type(name);
+    cy.get("input").eq(1).clear().type("1.23");
+    cy.get("input").eq(2).clear().type("34");
+
+    cy.contains("button", "Ajouter").click();
+  });
+});
