a510a817874a1581ea6a8961705f3e96485b2125
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-06-07 12:44:00 +0200

    dev: post article is working

--- back/api.js
+++ back/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const crypto = require("crypto");
 
 const app = express.Router();
 
@@ -13,6 +14,15 @@ app.get("/date", (req, res) => {
 
 app.get("/articles", (req, res) => {
   res.json(articles);
+});
+
+app.use(express.json());
+
+app.post("/articles", (req, res) => {
+  const newArticle = req.body;
+  const article = { ...newArticle, id: crypto.randomUUID() };
+  articles.push(article);
+  res.status(201).end();
 });
 
 module.exports = app;
